143d67bb99beaa9898f2272a84da7ca802e1399b
dev dz <beiniptv.tools@gmail.com>
2021-09-29 23:31:57 +0100

    update sonarlint2

--- vifbox/settings.py
+++ vifbox/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'vifApp',
     'phonenumber_field',
-    'storages'
+    # 'storages'
 ]
 
 MIDDLEWARE = [
@@ -157,16 +157,5 @@ SERVER_EMAIL = EMAIL_HOST_USER
 
 
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-AWS_S3_REGION_NAME = "ap-south-1"
-AWS_QUERYSTRING_EXPIRE = 604800
 
 
