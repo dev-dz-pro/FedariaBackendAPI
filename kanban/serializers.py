5b1c2e510da5657c56818018935ef683667a6d17
dev dz <beiniptv.tools@gmail.com>
2021-10-14 20:09:14 +0100

    github image fixxx

--- kanban/serializers.py
+++ kanban/serializers.py
@@ -24,7 +24,7 @@ class PPSerializer(serializers.ModelSerializer):
 class KanbanBoardSerializer(serializers.Serializer):
     portfolio = serializers.CharField(required=True)
     name = serializers.CharField(required=True)
-    projectdescription = serializers.CharField(required=False)
+    projectdescription = serializers.CharField(allow_blank=True)
     agileframwork = serializers.CharField(required=True)
     productowner = serializers.EmailField(max_length=None, min_length=None, allow_blank=True)
     scrummaster = serializers.EmailField(max_length=None, min_length=None, allow_blank=True)
